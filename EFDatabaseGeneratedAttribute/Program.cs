822e9b0243e94625f40b0e4f3434c74a9db34d2d
TekTutorialsHub <admin@TekTutorialsHub.com>
2016-01-18 11:14:48 +0530

    Created

---
+++ EFDatabaseGeneratedAttribute/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDatabaseGeneratedAttribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EFContext ctx = new EFContext();
+
+
+            //Deleting the exising data. Other wise primary key error is raised if you run it again
+            var all = from e in ctx.Employees select e;
+            ctx.Employees.RemoveRange(all);
+            ctx.SaveChanges();
+
+
+            //Inserting the data to employee Table
+            Employee emp = new Employee();
+            emp.EmployeeID = 1001;
+            emp.Name = "Test";
+            ctx.Employees.Add(emp);
+            ctx.SaveChanges();
+            Console.WriteLine("Added  SrNo " + emp.SrNo.ToString());
+
+            emp = new Employee();
+            emp.EmployeeID = 2001;
+            emp.Name = "Testing Again";
+            ctx.Employees.Add(emp);
+            ctx.SaveChanges();
+
+            Console.WriteLine("Added  SrNo " + emp.SrNo.ToString());
+
+
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+
+        }
+    }
+}
