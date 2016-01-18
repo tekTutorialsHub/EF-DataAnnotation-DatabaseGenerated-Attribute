80cf4d15af8163d61f2eb913d42224ff3114cb7f
TekTutorialsHub <admin@TekTutorialsHub.com>
2016-01-18 17:45:09 +0530

    Commit

---
+++ EFDatabaseGeneratedAttribute/EFContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EFDatabaseGeneratedAttribute
+{
+    public class EFContext : DbContext
+    {
+        public EFContext()
+            : base("EFDatabase")
+        {
+            Database.SetInitializer<EFContext>(new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Employee1> Employees1 { get; set; }
+        public DbSet<Employee2> Employees2 { get; set; }
+
+    }
+
+
+}
---
+++ EFDatabaseGeneratedAttribute/models.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFDatabaseGeneratedAttribute
+{
+
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]  
+        public int SrNo { get; set; }
+
+    }
+
+
+
+    public class Employee1
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]  
+        public int Employee1ID { get; set; }
+        public string Name { get; set; }
+
+    }
+
+
+    public class Employee2
+    {
+        
+        public int Employee2ID { get; set; }
+        public string Name { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public int? Amount { get; set; }
+    }
+
+}
